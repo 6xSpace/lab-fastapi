04270b3efc5eae416bb32f54eacfcf7af533fbfd
FSJagielski <f.s.jagielski@gmail.com>
2023-05-17 09:28:37 +0200

    essa sito

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    first_name: str
+    last_name: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/student")
+async def create_item(item: Item):
+    return item
